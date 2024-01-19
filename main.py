a27c0d1ad6d167ff22781ff0d97a03558d032336
JuanMaDev <86654681+0MFCAT@users.noreply.github.com>
2024-01-18 19:02:32 -0500

    first commit with basic stuff

---
+++ main.py
@@ -0,0 +1,21 @@
+import telebot
+
+# Crea una instancia del bot con tu token de API de Telegram
+bot_token = input('Introduce tu token de API de Telegram: ')
+bot = telebot.TeleBot('TOKEN_DEL_BOT')
+
+# Código para responder al mensaje /help
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    # Envía un mensaje de ayuda al usuario
+    bot.reply_to(message, "¡Hola! Soy utn bot de ayuda. ¿En qué puedo ayudarte?")
+
+
+# Código para responder a cualquier otro mensaje
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    # Responde al mensaje del usuario
+    bot.reply_to(message, "¡Hola! Soy un bot y estoy aquí para ayudarte.")
+
+# Inicia el bot
+bot.polling()
